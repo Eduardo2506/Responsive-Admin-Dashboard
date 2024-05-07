247e2771b49b39f6cfd8c4f5e4a5bb5be166bb04
Eduardo Castro <71782712+Eduardo2506@users.noreply.github.com>
2024-05-07 10:35:57 -0500

    NUEVO LOGO - INDEX - ORDERS - STYLE

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'JavaScript Course',
+        productNumber: '12034',
+        paymentStatus: 'Due',
+        status: 'Pending',
+    },
+    {
+        productName: 'React Course',
+        productNumber: '12035',
+        paymentStatus: 'Refunded',
+        status: 'Declined',
+    },
+    {
+        productName: 'Node Course',
+        productNumber: '12036',
+        paymentStatus: 'Paid',
+        status: 'Active',
+    }
+]
